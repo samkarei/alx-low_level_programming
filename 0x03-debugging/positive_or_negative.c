2835726529d60cac192086967e66da67ea867c80
root <samanthakarei2016@gmail.com>
2023-03-26 11:52:44 -0700

    ALX low level programming_Debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* postive_or_negative - checks for positive ornegative numbers,
+*
+* @i: The number to be checked.
+*
+* Return: Always 0.
+*/
+
+void positive_or_negative(int i)
+{
+if (i < 0)
+printf("%d is negative", i);
+else
+printf("%d is positive", i);
+}
